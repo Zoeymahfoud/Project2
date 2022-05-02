e9110e61f843de9d008581f6077c45dbe51edec9
Fernanda Albuquerque <fmahfouddealbuque@mail.valenciacollege.edu>
2022-05-01 21:34:22 -0400

    first commit

--- main.js
+++ main.js
@@ -2,37 +2,35 @@ const game = () => {
     let playerScore = 0;
     let computerScore = 0;
     let moves = 0;
- 
+}
 const playGame = () => {
     const rockBtn = document.querySelector('.rock');
     const paperBtn = document.querySelector('.paper');
     const scissorBtn = document.querySelector('.scissor');
     const playerOptions = [rockBtn,paperBtn,scissorBtn];
     const computerOptions = ['rock','paper','scissors']
-         
+}         
     playerOptions.forEach(option => {
     option.addEventListener('click',function(){
 if(playerScore > computerScore){
     result.style.fontSize = '2rem';
-    result.innerText = 'You Won The Game'
-    result.style.color = '#308D46';
+    result.innerText = 'You Won The Game';
 }
 else if(playerScore < computerScore){
     result.style.fontSize = '2rem';
     result.innerText = 'You Lost The Game';
-    result.style.color = 'red';
 }
 else{
     result.style.fontSize = '2rem';
     result.innerText = 'Tie';
-    result.style.color = 'grey'
 }
+
     reloadBtn.innerText = 'Restart';
-    reloadBtn.style.display = 'flex'
+    reloadBtn.style.display = 'flex';
     reloadBtn.addEventListener('click',() => {
     window.location.reload();
 })
-winner(this.innerText,computerChoice)
+winner(this.innerText,computerChoice);
 const winner = (player,computer) => {
 const result = document.querySelector('.result');
 const playerScoreBoard = document.querySelector('.p-count');
@@ -41,7 +39,7 @@ player = player.toLowerCase();
 computer = computer.toLowerCase();
     
 if(player === computer){
-result.textContent = 'Tie'
+result.textContent = 'Tie';
 }
 else if(player == 'rock'){
 if(computer == 'paper'){
@@ -50,7 +48,7 @@ if(computer == 'paper'){
     computerScoreBoard.textContent = computerScore;
  
 }else{
-    result.textContent = 'You Won'
+    result.textContent = 'You Won';
     playerScore++;
     playerScoreBoard.textContent = playerScore;
 }

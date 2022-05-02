d2a3a6c7514120beed4e6aa9023f6688b9364c58
Fernanda Albuquerque <fmahfouddealbuque@mail.valenciacollege.edu>
2022-05-01 21:01:38 -0400

    first commit

--- main.js
+++ main.js
@@ -12,49 +12,67 @@ const playGame = () => {
          
     playerOptions.forEach(option => {
     option.addEventListener('click',function(){
- 
+if(playerScore > computerScore){
+    result.style.fontSize = '2rem';
+    result.innerText = 'You Won The Game'
+    result.style.color = '#308D46';
+}
+else if(playerScore < computerScore){
+    result.style.fontSize = '2rem';
+    result.innerText = 'You Lost The Game';
+    result.style.color = 'red';
+}
+else{
+    result.style.fontSize = '2rem';
+    result.innerText = 'Tie';
+    result.style.color = 'grey'
+}
+    reloadBtn.innerText = 'Restart';
+    reloadBtn.style.display = 'flex'
+    reloadBtn.addEventListener('click',() => {
+    window.location.reload();
+})
 winner(this.innerText,computerChoice)
- 
-    const winner = (player,computer) => {
-    const result = document.querySelector('.result');
-    const playerScoreBoard = document.querySelector('.p-count');
-    const computerScoreBoard = document.querySelector('.c-count');
-    player = player.toLowerCase();
-    computer = computer.toLowerCase();
+const winner = (player,computer) => {
+const result = document.querySelector('.result');
+const playerScoreBoard = document.querySelector('.p-count');
+const computerScoreBoard = document.querySelector('.c-count');
+player = player.toLowerCase();
+computer = computer.toLowerCase();
     
 if(player === computer){
 result.textContent = 'Tie'
 }
 else if(player == 'rock'){
 if(computer == 'paper'){
-    result.textContent = 'Computer Won';
+    result.textContent = 'You Lose';
     computerScore++;
     computerScoreBoard.textContent = computerScore;
  
 }else{
-    result.textContent = 'Player Won'
+    result.textContent = 'You Won'
     playerScore++;
     playerScoreBoard.textContent = playerScore;
 }
 }
 else if(player == 'scissors'){
 if(computer == 'rock'){
-    result.textContent = 'Computer Won';
+    result.textContent = 'You Lose';
     computerScore++;
     computerScoreBoard.textContent = computerScore;
 }else{
-    result.textContent = 'Player Won';
+    result.textContent = 'You Won';
     playerScore++;
     playerScoreBoard.textContent = playerScore;
  }
 }
 else if(player == 'paper'){
     if(computer == 'scissors'){
-    result.textContent = 'Computer Won';
+    result.textContent = 'You Lose';
     computerScore++;
     computerScoreBoard.textContent = computerScore;
 }else{
-    result.textContent = 'Player Won';
+    result.textContent = 'You Won';
     playerScore++;
     playerScoreBoard.textContent = playerScore;
 }
@@ -67,26 +85,6 @@ const reloadBtn = document.querySelector('.reload');
  playerOptions.forEach(option => {
     option.style.display = 'none';
 })
- 
-      
-chooseMove.innerText = 'Game Over!!'
-    movesLeft.style.display = 'none';
- 
-if(playerScore > computerScore){
-result.style.fontSize = '2rem';
-result.innerText = 'You Won The Game'
-result.style.color = '#308D46';
-}
-else if(playerScore < computerScore){
-    result.style.fontSize = '2rem';
-    result.innerText = 'You Lost The Game';
-    result.style.color = 'red';
-}
-else{
-    result.style.fontSize = '2rem';
-    result.innerText = 'Tie';
-    result.style.color = 'grey'
-}
 reloadBtn.innerText = 'Restart';
 reloadBtn.style.display = 'flex'
 reloadBtn.addEventListener('click',() => {
@@ -96,4 +94,3 @@ reloadBtn.addEventListener('click',() => {
 playGame(); 
 }
 game());
-}
